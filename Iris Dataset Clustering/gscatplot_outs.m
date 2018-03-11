39c3fd38a072184b01c5cc7cf3331c32a7ccf540
Nikos Ar <nikos_rvnt@protonmail.com>
2018-03-11 23:52:25 +0200

    Update gscatplot_outs.m

--- Iris Dataset Clustering/gscatplot_outs.m
+++ Iris Dataset Clustering/gscatplot_outs.m
@@ -1,14 +1,16 @@
-
+% reading pc1.txt file
 filename = fullfile( 'IrisDataset_Clustering', 'IrisDataset_Clustering', 'pc1.txt') ;
 fileID = fopen(filename) ;
 C = textscan( fileID, '%f') ;
 fclose(fileID);
 
+% reading pc2.txt file
 filename2 = fullfile( 'IrisDataset_Clustering', 'IrisDataset_Clustering', 'pc2.txt');
 fileID2 = fopen(filename2);
 C2 = textscan(fileID2,'%f');
 fclose(fileID2);
 
+% reading labels.txt file
 filename3 = fullfile( 'IrisDataset_Clustering', 'IrisDataset_Clustering', 'labels.txt');
 fileID3 = fopen(filename3);
 C3 = textscan(fileID3,'%f');
@@ -18,6 +20,7 @@ pc1 = C{1,1}; % zeros(150,1);
 pc2 = C2{1,1}; % zeros(150,1)
 labels = C3{1,1};
 
+% gscatter plot - clustering of iris data on labels respectively
 figure(1) ;
 title('Red-Green-Blue in respect to Sentosa-Virginica-Versicolor');
 gscatter( pc1, pc2, labels, [], [], [], 'on', 'PC1', 'PC2') ;
